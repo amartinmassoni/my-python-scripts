ec1b9ee09bc524972510586e3506727369e88922
Aurelio Martin <amartinmassoni@gmail.com>
2020-11-14 21:33:00 +0100

    Processing dates and message parts

--- postfix_maillog_excel.py
+++ postfix_maillog_excel.py
@@ -2,21 +2,43 @@
 
 import openpyxl
 import logging
+import time
 import re
 
+class PostFixMessage:
+    def __init__( self, pf_id, timestamp ):
+        self.pf_id = pf_id
+        self.from_timestamp = timestamp
+        self.to_timestamp = timestamp
+        self.items = []
+
+    def add_line( self, timestamp, text ):
+        self.to_timestamp = max( self.to_timestamp, timestamp )
+        if text != "removed":
+            self.items.append( text.split( ", " ) )
+
 def postfix_maillog_excel( maillog, output ):
-    preffix = "^([A-Z][a-z][a-z] [0-9 ][0-9] [0-9]{2}:[0-9]{2}:[0-9]{2}) ([^ ]+) postfix/([a-z]+)\[([0-9]+)\]\: "
-    re_message = re.compile( preffix + "[0-9A-F]{10,12}\:" )
+    preffix = "^([A-Z][a-z][a-z] [0-9 ][0-9] [0-9]{2}:[0-9]{2}:[0-9]{2}) [^ ]+ postfix/[a-z]+\[[0-9]+\]\: "
+    re_message = re.compile( preffix + "([0-9A-F]{10,12}): (.+)$" )
     re_connect = re.compile( preffix + "connect from (.+)$" )
     re_disconnect = re.compile( preffix + "disconnect from (.+)$" )
     re_statistics = re.compile( preffix + "statistics: (.+)$" )
     re_timeout = re.compile( preffix + "(timeout after .+)$" )
     re_daemon = re.compile( preffix + "daemon started -- (.+)$" )
+    current_year = time.strftime( "%Y" ) + " "
+    pf_ids = []
+    pf_messages = {}
     for filename in sorted( maillog ):
         logging.debug( "Reading file %s...", filename )
         for line in open( filename ):
-            if re_message.search( line ):
-                pass
+            r1 = re_message.search( line )
+            if r1:
+                timestamp = time.strptime( current_year + r1.group( 1 ), "%Y %b %d %H:%M:%S" )
+                pf_id = r1.group( 2 )
+                if pf_id not in pf_messages:
+                    pf_ids.append( pf_id )
+                    pf_messages[ pf_id ] = PostFixMessage( pf_id, timestamp )
+                pf_messages[ pf_id ].add_line( timestamp, r1.group( 3 ) )
             elif re_connect.search( line ):
                 pass
             elif re_disconnect.search( line ):
@@ -30,6 +52,7 @@ def postfix_maillog_excel( maillog, output ):
             else:
                 print( line )
     logging.debug( "Saving to %s...", output )
+    return ( pf_ids, pf_messages )
 
 if __name__ == "__main__":
 
@@ -40,5 +63,4 @@ if __name__ == "__main__":
     parser.add_argument( "maillog", nargs = "+", help = "Input: maillog files" )
     parser.add_argument( "-o", "--output", help = "Output: Excel file", required = True )
     args = parser.parse_args()
-    postfix_maillog_excel( maillog = args.maillog, output = args.output )
-
+    pf_ids, pf_messages = postfix_maillog_excel( maillog = args.maillog, output = args.output )
