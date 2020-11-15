5e423e44b2e78d237761e6da0fb02d5f60d2619b
Aurelio Martin <amartinmassoni@gmail.com>
2020-11-15 17:31:05 +0100

    New class SmtpDelivery. Divide between processing the files, and creating the excel

--- postfix_maillog_excel.py
+++ postfix_maillog_excel.py
@@ -5,19 +5,41 @@ import logging
 import time
 import re
 
-class PostFixMessage:
-    def __init__( self, pf_id, timestamp ):
-        self.pf_id = pf_id
+class SmtpDelivery:
+    def __init__( self, items ):
+         self.to_ = None
+         self.relay = None
+         self.delay = None
+         self.delays = None
+         self.dsn = None
+         self.status = None
+
+class QueueMessage:
+    def __init__( self, queue_id, timestamp ):
+        self.queue_id = queue_id
         self.from_timestamp = timestamp
         self.to_timestamp = timestamp
+        self.client = None
+        self.message_id = None
+        self.from_ = None
+        self.size = None
+        self.nrcpt = None
+        self.removed = False
+        self.delivery = []
         self.items = []
 
     def add_line( self, timestamp, text ):
         self.to_timestamp = max( self.to_timestamp, timestamp )
-        if text != "removed":
-            self.items.append( text.split( ", " ) )
+        if text == "removed":
+            self.removed = True
+        else:
+            self.items.append( [ item.split( "=", 1 ) for item in text.split( ", " ) ] )
 
-def postfix_maillog_excel( maillog, output ):
+    def __repr__( self ):
+        return f'<QueueMessage {self.queue_id}: from {self.from_timestamp} to {self.to_timestamp} removed:{self.removed}>'
+
+
+def postfix_maillog( maillog ):
     preffix = "^([A-Z][a-z][a-z] [0-9 ][0-9] [0-9]{2}:[0-9]{2}:[0-9]{2}) [^ ]+ postfix/[a-z]+\[[0-9]+\]\: "
     re_message = re.compile( preffix + "([0-9A-F]{10,12}): (.+)$" )
     re_connect = re.compile( preffix + "connect from (.+)$" )
@@ -26,19 +48,19 @@ def postfix_maillog_excel( maillog, output ):
     re_timeout = re.compile( preffix + "(timeout after .+)$" )
     re_daemon = re.compile( preffix + "daemon started -- (.+)$" )
     current_year = time.strftime( "%Y" ) + " "
-    pf_ids = []
-    pf_messages = {}
+    queue_ids = []
+    queue_messages = {}
     for filename in sorted( maillog ):
         logging.debug( "Reading file %s...", filename )
         for line in open( filename ):
             r1 = re_message.search( line )
             if r1:
                 timestamp = time.strptime( current_year + r1.group( 1 ), "%Y %b %d %H:%M:%S" )
-                pf_id = r1.group( 2 )
-                if pf_id not in pf_messages:
-                    pf_ids.append( pf_id )
-                    pf_messages[ pf_id ] = PostFixMessage( pf_id, timestamp )
-                pf_messages[ pf_id ].add_line( timestamp, r1.group( 3 ) )
+                queue_id = r1.group( 2 )
+                if queue_id not in queue_messages:
+                    queue_ids.append( queue_id )
+                    queue_messages[ queue_id ] = QueueMessage( queue_id, timestamp )
+                queue_messages[ queue_id ].add_line( timestamp, r1.group( 3 ) )
             elif re_connect.search( line ):
                 pass
             elif re_disconnect.search( line ):
@@ -51,8 +73,18 @@ def postfix_maillog_excel( maillog, output ):
                 pass
             else:
                 print( line )
+    return ( queue_ids, queue_messages )
+
+def messages_to_excel( queue_ids, queue_messages, output ):
     logging.debug( "Saving to %s...", output )
-    return ( pf_ids, pf_messages )
+    wb = openpyxl.Workbook()
+    ws1 = wb.active
+    ws1.title = "Maillog"
+    for ( column_number, column_name ) in enumerate( [ "ID", "Client" ] ):
+        ws1.cell( column = column_number, row = 1, value = column_name )
+
+    wb.save( filename = output )
+
 
 if __name__ == "__main__":
 
@@ -63,4 +95,5 @@ if __name__ == "__main__":
     parser.add_argument( "maillog", nargs = "+", help = "Input: maillog files" )
     parser.add_argument( "-o", "--output", help = "Output: Excel file", required = True )
     args = parser.parse_args()
-    pf_ids, pf_messages = postfix_maillog_excel( maillog = args.maillog, output = args.output )
+    queue_ids, queue_messages = postfix_maillog( maillog = args.maillog )
+    messages_to_excel( queue_ids = queue_ids, queue_messages = queue_messages, output = args.output )
